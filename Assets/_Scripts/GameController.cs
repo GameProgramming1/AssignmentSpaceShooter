d5c4072bae1bda03f9c8c69d26c577410417a4b4
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-10-04 16:40:42 -0400

    added gameobject to handle number of enemies

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    //public instance variable
+    public int enemyNumber;
+    public GameObject enemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        this._GenerateEnemy();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //generate cloud
+    private void _GenerateEnemy()
+    {
+        for (int count = 0; count < this.enemyNumber; count++)
+        {
+            Instantiate(enemy);
+        }
+    }
+}
