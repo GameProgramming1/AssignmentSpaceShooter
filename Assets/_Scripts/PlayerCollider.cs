681ab37dd7dcd9382e46f8391d5cad915c0b3837
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-10-04 23:55:21 -0400

    added audio and text labels

--- Assets/_Scripts/PlayerCollider.cs
+++ Assets/_Scripts/PlayerCollider.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerCollider : MonoBehaviour {
 
     //public instance variables
-    //public Text scorelabel;
-    //public Text livesLabel;
-    //public Text gameOverLabel;
-    //public Text finalScoreLabel;
-    //public int _scoreValue = 0;
-    //public int _liveValue = 5;
+    public Text scorelabel;
+    public Text livesLabel;
+    public Text gameOverLabel;
+    public Text finalScoreLabel;
+    public int _scoreValue = 0;
+    public int _liveValue = 5;
 
     //private instance variable
     private AudioSource[] _audioSources; //array of audio sources
     private AudioSource _EnemyAudioSource;
     private AudioSource _PowerpointAudioSource;
-    //private AudioSource _EndingSound;
+    private AudioSource _EndingSound;
 
 
 
@@ -25,11 +26,11 @@ public class PlayerCollider : MonoBehaviour {
         this._audioSources = this.GetComponents<AudioSource>();
         this._EnemyAudioSource = this._audioSources[1];//reference cloud sound
         this._PowerpointAudioSource = this._audioSources[2];
-        //this._EndingSound = this._audioSources[3];
-        //this.gameOverLabel.enabled = false;
-        //this.finalScoreLabel.enabled = false;
+        this._EndingSound = this._audioSources[3];
+        this.gameOverLabel.enabled = false;
+        this.finalScoreLabel.enabled = false;
 
-        //this._setScore();
+        this._setScore();
     }
 
     // Update is called once per frame
@@ -43,39 +44,39 @@ public class PlayerCollider : MonoBehaviour {
         if (otherGameObject.tag == "Powerpill")
         {
             this._PowerpointAudioSource.Play(); //play yay sound
-            //this._scoreValue += 100; //add 100 points
+            this._scoreValue += 100; //add 100 points
         }
         else
         {
             this._EnemyAudioSource.Play();
-            //this._liveValue--; //remove 1 life
-            //if (this._liveValue <= 0)
-            //{
-            //    this._EndingSound.Play();
+            this._liveValue--; //remove 1 life
+            if (this._liveValue <= 0)
+            {
+                this._EndingSound.Play();
 
-            //    //yield return new WaitForSeconds(300);
-            //    this._EndGame();
-            //}
+                //yield return new WaitForSeconds(300);
+                this._EndGame();
+            }
 
         }
-        //this._setScore();
+        this._setScore();
     }
 
-    //private void _setScore()
-    //{
-    //    this.scorelabel.text = "Score : " + this._scoreValue;
-    //    this.livesLabel.text = "Lives : " + this._liveValue;
+    private void _setScore()
+    {
+        this.scorelabel.text = "Score : " + this._scoreValue;
+        this.livesLabel.text = "Lives : " + this._liveValue;
 
-    //}
+    }
 
-    //private void _EndGame()
-    //{
-    //    Destroy(gameObject);
-    ////    this.scorelabel.enabled = false;
-    ////    this.livesLabel.enabled = false;
-    ////    this.gameOverLabel.enabled = true;
-    ////    this.finalScoreLabel.enabled = true;
-    ////    this.finalScoreLabel.text = "Score :" + this._scoreValue;
+    private void _EndGame()
+    {
+        Destroy(gameObject);
+        this.scorelabel.enabled = false;
+        this.livesLabel.enabled = false;
+        this.gameOverLabel.enabled = true;
+        this.finalScoreLabel.enabled = true;
+        this.finalScoreLabel.text = "Score :" + this._scoreValue;
 
-    //}
+    }
 }
